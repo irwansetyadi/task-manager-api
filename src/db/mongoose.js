3e8897b17ea5010fd0c7ac71cb5aad58a4d176b2
Irwan Setyadi <irwan.setyadi@gmail.com>
2022-11-04 22:32:35 +0800

    set up for production

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb://admin:bismillah@ac-ajhhjc3-shard-00-00.ixivgab.mongodb.net:27017,ac-ajhhjc3-shard-00-01.ixivgab.mongodb.net:27017,ac-ajhhjc3-shard-00-02.ixivgab.mongodb.net:27017/task-manager-api?ssl=true&replicaSet=atlas-12sdti-shard-0&authSource=admin&retryWrites=true&w=majority');
+mongoose.connect(process.env.MONGODB_URL);
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const taskRouter = require('./routers/task');
 //activating express
 const app = express();
 //Setting up port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 //to convert incoming request into JSON format
 app.use(express.json());
@@ -18,24 +18,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
-
-
-
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// const main = async () => {
-//     // const task = await Task.findOne({_id:'6358042ce4c0960429fdd30f'}).populate('author').exec(function(err, task){
-//     //     console.log(task.author.name);
-//     // })
-
-//     // // console.log(task.author);
-//     const user = await User.findById('6358041de4c0960429fdd30a').populate('tasks').exec((err, user) =>{
-//         if(err) console.error(err);
-
-//         console.log(user.tasks)
-//     });
-// }
-
-// main()
